1be79c7a22420ad287f30896b164e8f07998e111
Andy Nur <andylulz@yahoo.com>
2017-10-18 22:05:06 +0700

    new module pengumuman

---
+++ app/controllers/PengumumanController.php
@@ -0,0 +1,205 @@
+<?php
+
+use Phalcon\Mvc\View;
+use Phalcon\Validation;
+use Phalcon\Validation\Validator\PresenceOf;
+use Phalcon\Mvc\Url;
+use Phalcon\Mvc\Model\Query\Builder;
+use Phalcon\Image\Adapter\Imagick;
+
+class PengumumanController extends \Phalcon\Mvc\Controller
+{
+    protected $check;
+    protected $messages;
+    protected $title;
+    protected $type;
+    protected $text;
+
+    public function initialize()
+    {
+        if (empty($this->session->get('uid'))) {
+            $this->response->redirect('account/loginEnd');
+        }
+        
+        $this->view->setRenderLevel(View::LEVEL_ACTION_VIEW);
+    }
+
+    public function indexAction()
+    {
+        $data = $this->modelsManager->createBuilder()
+                ->addFrom('RefPengumuman', 'p')
+                ->join('ViewUser', 'p.pengirim_uid = u.login', 'u')
+                ->join('RefRombonganBelajar', 'p.tujuan = r.rombongan_belajar_id', 'r')
+                ->join('RefTingkatPendidikan', 'r.tingkat_pendidikan_id = t.tingkat_pendidikan_id', 't')
+                ->columns(['p.pengumuman_id', 'p.judul', 'p.isi', 'p.tanggal', 'u.login', 'u.nama', 'u.foto', 'p.tujuan', 'p.lampiran', 'p.status', 'r.nama AS nama_rombel', 't.nama AS nama_tingkat'])
+                ->orderBy('p.pengumuman_id DESC')
+                ->getQuery()
+                ->execute();
+
+        $rombel = $this->modelsManager->createBuilder()
+                ->addFrom('RefRombonganBelajar', 'r')
+                ->join('RefTingkatPendidikan', 'r.tingkat_pendidikan_id = t.tingkat_pendidikan_id', 't')
+                ->columns(['r.rombongan_belajar_id AS id', 'r.nama', 't.nama AS tingkat'])
+                ->orderBy('t.nama ASC')
+                ->getQuery()
+                ->execute();                
+
+        $this->view->setVars([
+            "data" => $data,
+            "rombel" => $rombel
+        ]);
+
+        $this->view->pick('pengumuman/index');
+    }
+
+    public function getPengumumanAction($id)
+    {
+        $this->view->disable();
+
+        $data = RefPengumuman::findFirst($id);
+
+        echo json_encode($data);
+    }
+
+    public function addPengumumanAction()
+    {        
+        $this->checkValidation();
+        
+        if (count($this->messages) == 0) {
+            $data = new RefPengumuman();
+            $this->save($data, 'tambah');        
+        }
+        
+        $notif = ['title' => $this->title, 'text' => $this->text, 'type' => $this->type];
+        echo json_encode($notif);        
+    }
+
+    public function editPengumumanAction($id)
+    {
+        $this->checkValidation();
+        
+        if (count($this->messages) == 0) {
+            $data = RefPengumuman::findFirst($id);
+            $this->save($data, 'ubah');
+        }
+
+        $notif = ['title' => $this->title, 'text' => $this->text, 'type' => $this->type];
+        echo json_encode($notif);     
+    }    
+
+    public function deletePengumumanAction($id)
+    {
+        $this->view->setRenderLevel(View::LEVEL_ACTION_VIEW);
+
+        $data = RefPengumuman::findFirst($id);
+        $data->delete();
+
+        echo json_encode(["status" => true]);
+    }
+
+    public function save($data, $message) 
+    {
+        $img_path = DOCUMENT_ROOT. 'img/pengumuman/';
+        $foto_lama = $_POST['foto_lama'];     
+        
+        if ($this->request->hasFiles()) {
+            foreach ($this->request->getUploadedFiles() as $file) {
+                // check if file is uploaded
+                if ($file->getSize() > 0) {                    
+                    // add new image
+                    $ext = $file->getExtension();
+                    $nama_file = md5(uniqid(rand(), true)) . '.' . $ext;
+                    $path_file = $img_path . $nama_file;   
+
+                    if ($this->imageCheck($file->getRealType())) {
+                        if ($file->moveTo($path_file)) {
+                            // resize image to 600px
+                            $image = new Imagick($path_file);
+                            $image->resize(
+                                600,
+                                null,
+                                \Phalcon\Image::WIDTH
+                            )->save();                               
+
+                            $foto = $nama_file;
+                        } else {                    
+                            die('gagal masuk bro!');
+                        }
+                    }
+                } else {
+                    $foto = '';                
+
+                    if ($message == 'ubah') {
+                        $foto = $foto_lama;
+                    }
+                }
+            }
+        }
+
+        $data->assign([
+            'judul' => $_POST['judul'],
+            'isi' => $_POST['isi'],
+            'tanggal' => $_POST['tgl_kirim'],
+            'pengirim_uid' => $this->session->get('uid'),
+            'tujuan' => $_POST['tujuan'],
+            'lampiran' => $foto,
+            'status' => $_POST['status'],
+        ]);
+        
+        if ($data->save()) {
+            // remove old image                  
+            if ($foto != $foto_lama) {
+                unlink($img_path . $foto_lama);
+            }            
+
+            $this->title = 'Sukses';
+            $this->text = 'Data berhasil di' . $message;
+            $this->type = 'success';
+        } else {
+            $errors = $data->getMessages();
+            foreach ($errors as $error) {
+                $this->text .= "$error"."</br>";
+            }
+            $this->title = 'Error!';
+            $this->type = 'warning';
+        }         
+    }   
+
+    public function presenceOf($validation, $column, $name) 
+    {
+        $validation->add($column, new PresenceOf([
+            'message' => '<b>'.$name.'</b> tidak boleh kosong'
+        ]));
+    }
+
+    public function checkValidation() 
+    {
+        $check = new Validation();
+ 
+        $this->presenceOf($check, 'judul', 'Judul Pengumuman');        
+        $this->presenceOf($check, 'isi', 'Isi Pengumuman');        
+        $this->presenceOf($check, 'tujuan', 'Tujuan');        
+        $this->presenceOf($check, 'tanggal', 'Tanggal Kirim');        
+        $this->presenceOf($check, 'status', 'Status');        
+
+        $this->messages = $check->validate($_POST);
+
+        foreach ($this->messages as $message) {
+            $this->text .= "$message"."</br>";
+        }
+        $this->title = 'Gagal';
+        $this->type = 'warning';  
+    }  
+
+    private function imageCheck($extension)
+    {
+        $allowedTypes = [
+            'image/gif',
+            'image/jpg',
+            'image/png',
+            'image/jpeg'
+        ];
+
+        return in_array($extension, $allowedTypes);
+    }    
+}
---
+++ app/views/pengumuman/assets/pengumuman.js
@@ -0,0 +1,145 @@
+(function (document, window, $) {
+
+    // Set page title
+    setPageTitle("Pengumuman");
+
+    // Define variables
+    var pageUrl         = "{{ url('pengumuman') }}",
+        imgPath         = "{{ url('img/pengumuman/') }}",
+        pageReload      = "pengumuman/index";
+
+    // Form elements object
+    var $form           = $("#form_input"),   
+        $title          = $("#form_title"),   
+        $delete         = $(".delete"),
+        $edit           = $(".edit"),
+        $submit         = $("#submit"),
+        $reset          = $("#reset"),
+        $showImage      = $(".show_image"),
+        $imagePreview   = $("#uploadPreview");
+
+    // Input elements object            
+    var $judul          = $('input[name=judul]'),
+        $tanggal        = $('input[name=tanggal]'),
+        $tglKirim       = $('input[name=tgl_kirim]'),
+        $isi            = $('textarea[name=isi]'),
+        $foto           = $('input[name=foto]'),
+        $fotoLama       = $('input[name=foto_lama]'),
+        $tujuan         = $('select[name=tujuan]'),
+        $draft          = $('input[name=status][value=draft]'),
+        $publish        = $('input[name=status][value=publish]');
+
+    // Image preview when upload button click
+    $foto.on("change", function() {
+        previewImage("uploadImage", "uploadPreview");
+    });
+
+    // ImageModal show when image click
+    $showImage.on("click", function() {
+        var $dataId = $('#' + $(this).closest('tr').attr('id'));
+        var judul = $dataId.find('td').eq(1).html();     
+        var imgSource = $(this).attr('data-image');
+
+        $('#imageModal').modal('show');            
+        $('#imageModalLabel').html(judul);
+        $('#imageModalSource').attr('src', imgPath + imgSource);
+    });
+
+    // Save data when submit
+    $form.on("submit", function(e) {
+        var saveUrl = $(this).prop('action');            
+        
+        saveData(this, saveUrl, pageReload);
+        e.preventDefault();            
+    });
+        
+    // Set data from table to form when edit
+    $edit.on("click", function() {
+        var editId      = $(this).attr("data-id"),
+            editUrl     = pageUrl + '/editPengumuman/' + editId,
+            getUrl      = pageUrl + '/getPengumuman/' + editId,
+            dataSend    = "id="+editId,
+            _no         = $('#data_' + editId).find('td').eq(0).html();
+
+        var getData = $.ajax({
+            type: 'POST',
+            url: getUrl,
+            dataType: 'json',
+            data: dataSend
+        });
+    
+        getData.done(function(response) {            
+            $judul.val(response.judul).focus();
+            $isi.val(response.isi);
+            $tujuan.val(response.tujuan);
+            $tanggal.val( tanggalIndonesia(response.tanggal) );
+            $tglKirim.val(response.tanggal);
+            checkedRadio(response.status, 'draft', $draft, $publish);
+            $fotoLama.val(response.lampiran);
+            
+            if (response.lampiran != '') {
+                $imagePreview.attr('src', imgPath + response.lampiran);
+            } else {
+                $imagePreview.attr('src', 'img/user.png');
+            }
+        });            
+    
+        $form.attr('action', editUrl);
+        $title.text('Ubah Data #' + _no);
+        $reset
+            .removeClass('btn-default').addClass('btn-danger')
+            .html('<i class="fa fa-times"></i>&nbsp; Batal');
+        $submit
+            .removeClass('btn-primary').addClass('btn-success')
+            .html('<i class="fa fa-send"></i>&nbsp; Update');
+    });
+
+    // Delete data when confirmed
+    $delete.on("click", function() {
+        var deleteId = $(this).attr("data-id"),
+            deleteUrl = pageUrl + '/deletePengumuman/' + deleteId;
+
+        deleteData(deleteUrl, pageReload);
+    });
+
+    // Reset or cancel form
+    $reset.on("click", function() {
+        $form.attr('action', pageUrl + '/addPengumuman');
+        $title.text('Tambah Data');
+        $imagePreview.attr('src', 'img/user.png');
+        $fotoLama.val('');
+        $reset
+            .removeClass('btn-danger').addClass('btn-default')
+            .html('<i class="fa fa-refresh"></i>&nbsp; Reset');
+        $submit
+            .removeClass('btn-success').addClass('btn-primary')
+            .html('<i class="fa fa-send"></i>&nbsp; Simpan');
+    });        
+
+    // Intialize datatable
+    dataTableConfig();
+
+    // Intialize filestyle
+    $(":file").filestyle({
+        input: false,
+        buttonText: "Upload Lampiran",
+        buttonName: "btn-danger",            
+        iconName: "fa fa-cloud-upload"
+    });    
+
+    // Intialize datepicker
+    $tanggal.datepicker({
+        language: 'id',
+        format: 'dd-MM-yyyy',
+        autoclose: true,
+        startDate: `-1y`,
+        endDate: '+1y',
+        todayBtn: true,
+        todayHighlight: true,
+        title: "Pilih Tanggal"
+    }).on('changeDate', function (ev) {
+        var selectedDate = ev.format(0, "yyyy-mm-dd");
+        $tglKirim.val(selectedDate);
+    });
+
+}(document, window, jQuery));
